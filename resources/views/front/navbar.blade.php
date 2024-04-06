2c59e0d4d99598e65e890e0944a7d46d56d0f310
Teo Albert <112470991+teoezekiel@users.noreply.github.com>
2024-04-06 14:17:14 +0630

    updated flags

--- resources/views/front/navbar.blade.php
+++ resources/views/front/navbar.blade.php
@@ -1,3 +1,29 @@
+<style>
+        /* Style for navigation links */
+        .navlinks li:hover,
+        .navlinks li.active {
+            background-color: #f0f0f0; /* Change to the desired hover color */
+        }
+
+        /* Style for active link */
+        .navlinks li.active {
+            font-weight: bold; /* Change to the desired active link style */
+        }
+
+        /* Style for anchor tags */
+        .navlinks li a {
+            display: block;
+            padding: 10px 15px;
+            text-decoration: none;
+            color: #333; /* Change to the desired text color */
+        }
+
+        /* Style for anchor tags on hover */
+        .navlinks li a:hover {
+            color: #000; /* Change to the desired text color on hover */
+        }
+
+</style>   
 <header>
         <nav class="nav affix">
             <div class="menu_containter ContainerUnactive">
@@ -9,15 +35,17 @@
                             <li class="{{ (request()->is('trandictoinal')? 'active' : '' ) }} ">
                                 <a href="{{ url('/trandictoinal') }}">カンボジアの伝統料理</a>
                             </li>
-                        <li class="{{ (request()->is('phumasia-activity')? 'active' : '' ) }}"><a href="{{ url('/phumasia-activity') }}">クイアクティビティ</a></li>
-                        <li><a href="{{ url('welcomepage') }}">プーマシア活動</a></li>
-                        <li><a href="#">環境</a></li>
-                        <li><a href="#">起業家精神</a></li>
-                        <li><a href="#">コラボレーション</a></li>
+                            <li class="{{ (request()->is('phumasia-activity')? 'active' : '' ) }}">
+                                <a href="{{ url('/phumasia-activity') }}">クイアクティビティ</a>
+                            </li>
+                            <li><a href="{{ url('welcomepage') }}">プーマシア活動</a></li>
+                            <li><a href="#">環境</a></li>
+                            <li><a href="#">起業家精神</a></li>
+                            <li><a href="#">コラボレーション</a></li>
                     </ul>
                 </div>
                 <div class="d-flex">
-                    <a href="#"><div class="page_darection"><div class="sybol" style="background-image: url({{ asset('img/Flag_of_Japan.svg.webp') }});"></div></div></a>
+                    <a href="#"><div class="page_darection"><div class="sybol" style="background-image: url({{ asset('img/Flag_of_Cambodia.png') }});"></div></div></a>
                     <a href="#"><div class="page_darection un_active"><div class="sybol" style="background-image: url({{ asset('img/Flag_of_Myanmar.svg.png') }});"></div></div></a>
                 </div>        
             </div>
